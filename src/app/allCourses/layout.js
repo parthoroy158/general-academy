0b6b7d8d18c88ddd276c7f358c58e7de6504b1cb
Partho Protim Roy <parthoroy158@gmail.com>
2025-07-17 10:08:18 +0600

    navbar fixed

--- src/app/allCourses/layout.js
+++ src/app/allCourses/layout.js
@@ -4,9 +4,9 @@ import AllCoursesNav from "./components/AllcoursesNav";
 export default function DashboardLayout({ children }) {
     return (
         <div className="pt-16 min-h-screen bg-gray-50 dark:bg-gray-50">
-            <div className="max-w-7xl mx-auto w-full bg-gray-50 ">
+            <div className="max-w-7xl mx-auto bg-gray-50 ">
                 {/* Sidebar */}
-                <aside className="w-full max-w-7xl mx-auto lg:w-64 bg-base-100 dark:bg-gray-50 border-b lg:border-b-0 lg:border-r border-none dark:border-black px-4 py-6">
+                <aside >
                     <AllCoursesNav></AllCoursesNav>
                 </aside>
 
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Banner from "./components/Banner";
 import Body from "./components/Body";
 import MentorsDetails from "./components/MentorsDetails";
+import AllCoursesNav from "./allCourses/components/AllcoursesNav";
 
 
 export default function Home() {
